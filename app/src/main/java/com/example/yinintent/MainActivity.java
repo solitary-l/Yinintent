c4914b61b23ac43ce90205a8589e00dcd270686b
solitary-l <55885022+solitary-l@users.noreply.github.com>
2019-11-17 21:21:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yinintent/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.yinintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main
+        );
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:18210991657"));
+                startActivity(intent);
+            }
+
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Intent.ACTION_VIEW);
+                intent1.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent1);
+            }
+
+        });
+
+    }
+
+}
+
